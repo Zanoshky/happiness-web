b8df21cb8dcbdbd308b6a9fe17aff5cdfdf859e2
Marko Zanoski <marko.zanoski@mysugr.com>
2019-11-26 15:26:47 +0100

    Move calculation to api

--- src/views/HomebaseHistory/HomebaseHistory.tsx
+++ src/views/HomebaseHistory/HomebaseHistory.tsx
@@ -1,26 +1,13 @@
 import React from "react";
 import SimpleChart, { ComponentDataType } from "../../components/SimpleChart/SimpleChart";
 
-type Status = {
-  happiness: number;
-  homebaseId: number;
-  timestamp: number;
-  humidity: number;
-  temperature: number;
-  dust: number;
-  gas: number;
-  volume: number;
-  light: number;
-};
-
-type MyState = { statuses: Array<Status> };
+type MyState = { statuses: Array<ComponentDataType> };
 
 class HomebaseHistory extends React.Component<{}, MyState> {
   intervalID: any;
 
   constructor(props: any) {
     super(props);
-    this.state = { statuses: [] };
   }
 
   async getLatestData() {
@@ -48,113 +35,23 @@ class HomebaseHistory extends React.Component<{}, MyState> {
     clearInterval(this.intervalID);
   }
 
-  getLightData() {
-    const statuses = [];
-    const data = { id: "Light", data: Array<any>() };
-
-    data["id"] = "Light";
-    this.state.statuses.forEach(status => {
-      const stat = {
-        x: new Date(new Date(status.timestamp).getTime()).toTimeString().split(" ")[0],
-        y: status.light
-      };
-      console.log(stat);
-      data.data.push(stat);
-    });
-
-    statuses.push(data);
-    return statuses;
-  }
-
-  getVolumeData() {
-    const statuses = [];
-    const data = { id: "Volume", data: Array<any>() };
-
-    data["id"] = "Volume";
-    this.state.statuses.forEach(status => {
-      const stat = { x: new Date(new Date(status.timestamp).getTime()).toTimeString().split(" ")[0], y: status.volume };
-      data.data.push(stat);
-    });
-
-    statuses.push(data);
-    return statuses;
-  }
-
-  getTemperatureData() {
-    const statuses = [];
-    const data = { id: "Temperature", data: Array<any>() };
-
-    data["id"] = "Temperature";
-    this.state.statuses.forEach(status => {
-      const stat = {
-        x: new Date(new Date(status.timestamp).getTime()).toTimeString().split(" ")[0],
-        y: status.temperature
-      };
-      data.data.push(stat);
-    });
-
-    statuses.push(data);
-    return statuses;
-  }
-
-  getHumidityData() {
-    const statuses = [];
-    const data = { id: "Humidity", data: Array<any>() };
-
-    data["id"] = "Humidity";
-    this.state.statuses.forEach(status => {
-      const stat = {
-        x: new Date(new Date(status.timestamp).getTime()).toTimeString().split(" ")[0],
-        y: status.humidity
-      };
-      data.data.push(stat);
-    });
-
-    statuses.push(data);
-    return statuses;
-  }
-
-  getDustData() {
-    const statuses = [];
-    const data = { id: "Dust", data: Array<any>() };
-
-    data["id"] = "Dust";
-    this.state.statuses.forEach(status => {
-      const stat = { x: new Date(new Date(status.timestamp).getTime()).toTimeString().split(" ")[0], y: status.dust };
-      data.data.push(stat);
-    });
-
-    statuses.push(data);
-    return statuses;
-  }
-
-  getGasData() {
-    const statuses = [];
-    const data = { id: "Gas", data: Array<any>() };
-
-    data["id"] = "Gas";
-    this.state.statuses.forEach(status => {
-      const stat = { x: new Date(new Date(status.timestamp).getTime()).toTimeString().split(" ")[0], y: status.gas };
-      data.data.push(stat);
-    });
-
-    statuses.push(data);
-    return statuses;
-  }
-
   render = () => {
+    if (this.state === null || this.state.statuses === null) {
+      return <div></div>;
+    }
+
     return (
       <div>
         <div>TestCharts</div>
         <section className={"nowItems"}>
-          <SimpleChart data={this.getLightData()} name={"Light"} />
-          <SimpleChart data={this.getVolumeData()} name={"Volume"} />
-          <SimpleChart data={this.getTemperatureData()} name={"Temperature"} />
+          <SimpleChart data={this.state.statuses[0]} name={"Light"} />
+          <SimpleChart data={this.state.statuses[1]} name={"Volume"} />
+          <SimpleChart data={this.state.statuses[2]} name={"Temperature"} />
         </section>
         <section className={"nowItems"}>
-          <SimpleChart data={this.getHumidityData()} name={"Humidity"} />
-          <SimpleChart data={this.getDustData()} name={"Dust"} />
-          <SimpleChart data={this.getGasData()} name={"Gas"} />
+          <SimpleChart data={this.state.statuses[5]} name={"Humidity"} />
+          <SimpleChart data={this.state.statuses[3]} name={"Dust"} />
+          <SimpleChart data={this.state.statuses[4]} name={"Gas"} />
         </section>
       </div>
     );
