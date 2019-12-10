72ccde8e7bb1f068eb2284bfd441500e65b8b90c
Marko Zanoski <marko.zanoski@mysugr.com>
2019-12-10 19:26:14 +0100

    Bump version

--- src/views/Dashboard/Dashboard.tsx
+++ src/views/Dashboard/Dashboard.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Button } from "antd";
 
+const pjson = require("./../../../package.json");
+
 const Dashboard: React.FC = () => {
   return (
     <div>
@@ -9,6 +11,10 @@ const Dashboard: React.FC = () => {
       <Button type="primary" size={"large"} href="/homebase/1">
         PROTOTYPE
       </Button>
+      <br />
+      <br />
+      <br />
+      <h3>Version: {pjson.version}</h3>
     </div>
   );
 };
